e393a5eeb1a34ab6c5ab0baf115b482ac002dee0
GerHawX <pschumann@jobrouter.de>
2020-02-25 13:13:16 +0100

    Grundstruktur index.php aufgebaut.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php 
+<html>
+<head>
+    <header>KI-Emotionserkennung</header>
+</head>
+<body>
+    <H1>KI-Emotionserkennung</H1>
+    <?php echo "Hallo Welt!"; ?>
+</body>
+</html>
 
-echo:"Hallo Welt!";
+
