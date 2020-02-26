845997adc16584ff16a35cf317c30d9d097ae341
GerHawX <pschumann@jobrouter.de>
2020-02-26 21:05:13 +0100

    Eigene CSS-Anweisungen in ownStyle.css ausgelagert.
    showImage.js in ownFunctions.js umbenannt, da mehrere Funktionen hier ausgeführt werden.
    
    Formatierung der angezeigten Bilder und des Textoutputs mittels HTML-Tabellen durch Formatierung mittels CSS-Anweisungen ersetzt.

--- assets/js/ownFunctions.js
+++ assets/js/ownFunctions.js
@@ -4,9 +4,19 @@ function showImage(pictureId) {
     switch (pictureId) {
         case "inputImage":
             sourceImageUrl = document.getElementById(pictureId).value;
+            doRestCall("Externes Bild");
             break;
         default:
             sourceImageUrl = document.getElementById(pictureId).src;
+            doRestCall(pictureId);
     }
     document.querySelector("#sourceImage").src = sourceImageUrl;
-};
+
+}
+
+function doRestCall(data) {
+    var responseTextArea = document.getElementById("responseTextArea");
+
+    responseTextArea.value = data + " geladen!";
+
+}
